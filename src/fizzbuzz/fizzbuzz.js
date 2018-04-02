851c2b671ec9bd4b66aee2de89398797c75327f0
Koirala <Kiran.Koirala@solera.com>
2018-04-02 18:03:43 -0500

    Completed FizzBuzz Kata

---
+++ src/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,12 @@
+class FizzBuzz {
+    
+    getFizzBuzz(input){
+        if (input%3 == 0 && input%5 == 0){return "FizzBuzz"}
+        if (input%3 == 0){return "Fizz"}
+        if (input%5 == 0){return "Buzz"}
+        return input;
+
+    }
+}
+
+export default FizzBuzz;
--- src/fizzbuzz/fizzbuzz.test.js
+++ src/fizzbuzz/fizzbuzz.test.js
@@ -1,12 +1,46 @@
 import {assert} from 'chai';
+import FizzBuzz from './FizzBuzz';
 
-describe('the environment', () => {
-  it('works, hopefully', () => {
-        assert.equal(1,1);    
+describe('getFizzbuzz', () => {
+  it('returns 1', () => {
+    var expected_result = 1;
+    
+    var newFizz = new FizzBuzz();
+
+    var result = newFizz.getFizzBuzz(expected_result);
+    assert.equal(result,expected_result);    
   });
-});
 
+  it('returns 2', () => {
+    var expected_result = 2;
+    
+    var newFizz = new FizzBuzz();
 
-// describe('getFizzBuzz returns number', function(){
-//     expect(true).to_be_true;
-// });
+    var result = newFizz.getFizzBuzz(expected_result);
+    assert.equal(result,expected_result);    
+  });
+
+  [3,6].forEach(function(value) {
+      it('returns Fizz for number divisible by 3 ' + value, function() {
+        var newFizz = new FizzBuzz();
+        var result = newFizz.getFizzBuzz(value);
+        assert.equal(result,"Fizz");    
+      });
+  });
+
+  [5,10].forEach(function(value) {
+    it('returns Buzz for number divisible by 5' + value, function() {
+      var newFizz = new FizzBuzz();
+      var result = newFizz.getFizzBuzz(value);
+      assert.equal(result,"Buzz");    
+    });
+  });
+
+    [15,30, 45].forEach(function(value) {
+      it('returns FizzBuzz for number divisible by 3 and 5' + value, function() {
+        var newFizz = new FizzBuzz();
+        var result = newFizz.getFizzBuzz(value);
+        assert.equal(result,"FizzBuzz");    
+      });
+    });
+})
