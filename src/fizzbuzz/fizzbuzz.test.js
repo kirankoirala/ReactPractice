80b664ce7b1ba073d1bf8fd382efc4a85e98a0e2
Koirala <Kiran.Koirala@solera.com>
2018-04-02 17:20:47 -0500

    added chai to test

---
+++ src/fizzbuzz/fizzbuzz.test.js
@@ -0,0 +1,12 @@
+import {assert} from 'chai';
+
+describe('the environment', () => {
+  it('works, hopefully', () => {
+        assert.equal(1,1);    
+  });
+});
+
+
+// describe('getFizzBuzz returns number', function(){
+//     expect(true).to_be_true;
+// });
