a8f33d38dd2ccd24647f0b0a877e5fd79542fad3
NA\Kiran.Koirala <kiran.koirala@solera.com>
2018-04-11 16:41:56 -0500

    starting fresh

--- src/fizzbuzz/fizzbuzz.js
+++ src/fizzbuzz/fizzbuzz.js
@@ -2,12 +2,7 @@ class FizzBuzz {
     
     getFizzBuzz(input){
 
-        return (input%3 == 0 && input % 5 == 0)?"FizzBuzz":(input%3==0?"Fizz":(input%5==0?"Buzz":input));
-        // if (input%3 == 0 && input%5 == 0){return "FizzBuzz"}
-        // if (input%3 == 0){return "Fizz"}
-        // if (input%5 == 0){return "Buzz"}
-        //return input;
-
+       
     }
 }
 
--- src/fizzbuzz/fizzbuzz.test.js
+++ src/fizzbuzz/fizzbuzz.test.js
@@ -2,46 +2,5 @@ import {assert, expect} from 'chai';
 import FizzBuzz from './FizzBuzz';
 
 describe('getFizzbuzz', () => {
-  it('returns 1', () => {
-    var expected_result = 1;
-    
-    var newFizz = new FizzBuzz();
-
-    var result = newFizz.getFizzBuzz(expected_result);
-   // assert.equal(result,expected_result);  
-    expect(result).to.eq(expected_result)  ;
-  });
-
-  it('returns 2', () => {
-    var expected_result = 2;
-    
-    var newFizz = new FizzBuzz();
-
-    var result = newFizz.getFizzBuzz(expected_result);
-    assert.equal(result,expected_result);    
-  });
-
-  [3,6,9].forEach(function(value) {
-      it('returns Fizz for number divisible by 3 ' + value, function() {
-        var newFizz = new FizzBuzz();
-        var result = newFizz.getFizzBuzz(value);
-        assert.equal(result,"Fizz");    
-      });
-  });
-
-  [5,10, 20].forEach(function(value) {
-    it('returns Buzz for number divisible by 5' + value, function() {
-      var newFizz = new FizzBuzz();
-      var result = newFizz.getFizzBuzz(value);
-      assert.equal(result,"Buzz");    
-    });
-  });
-
-    [15,30, 45].forEach(function(value) {
-      it('returns FizzBuzz for number divisible by 3 and 5' + value, function() {
-        var newFizz = new FizzBuzz();
-        var result = newFizz.getFizzBuzz(value);
-        assert.equal(result,"FizzBuzz");    
-      });
-    });
+  
 })
